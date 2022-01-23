1326ee774affc52bc8f9608ed0995cbb5528bd0e
daniel deleon <dviana7898@gmail.com>
2022-01-22 23:57:32 -0500

    Arreglamos nombre de carpeta heroes

--- src/app/components/heroe-tarjeta/heroe-tarjeta.component.ts
+++ src/app/components/heroe-tarjeta/heroe-tarjeta.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-heroe-tarjeta',
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroeTarjetaComponent implements OnInit {
 
+  @Input() heroe: any = {};
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  verheroe() {
+    
   }
 
 }
